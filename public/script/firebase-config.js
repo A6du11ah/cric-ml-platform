dab16ab7805427bbafc663c4c2a4d1c38627ca91
amina2155 <98386611+amina2155@users.noreply.github.com>
2024-07-04 13:59:07 +0600

    Amina created Login.js and a dummy profile page to test Database connection also done

---
+++ public/script/firebase-config.js
@@ -0,0 +1,23 @@
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBcVp-XOWgLhPxh82PLT1vbnAPxPpTXIb4",
+
+  authDomain: "mychat-793a5.firebaseapp.com",
+
+  databaseURL: "https://mychat-793a5-default-rtdb.firebaseio.com",
+
+  projectId: "mychat-793a5",
+
+  storageBucket: "mychat-793a5.appspot.com",
+
+  messagingSenderId: "1079208780500",
+
+  appId: "1:1079208780500:web:b26695d70b892fb5285141",
+
+  measurementId: "G-150SHR7M14"
+
+
+};
+
+  
+firebase.initializeApp(firebaseConfig);
---
+++ public/script/login.js
@@ -0,0 +1,38 @@
+document.getElementById('login-with-google-button').addEventListener('click', async () => {
+    try {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        const result = await firebase.auth().signInWithPopup(provider);
+
+        const fullName = result.user.displayName;
+
+        const userData = {
+            email: result.user.email,
+            user_name: fullName,
+            picture_ref: result.user.photoURL,
+            user_role: 'user', // Assuming default role is 'user', adjust as necessary
+            provider: 'google' // As we're using Google Sign-In
+        };
+
+        // Save user data in PostgreSQL
+        const response = await fetch('http://localhost:3000/api/createUser', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userData)
+        });
+
+        const data = await response.json();
+
+        if (response.status === 201) {
+            console.log('New user created:', data);
+        } else if (response.status === 200 && data.message === 'User already exists') {
+            console.log('User already exists');
+        }
+
+        console.log('Redirecting to profile.html');
+        window.location.href = 'http://localhost:3000/profile';
+    } catch (error) {
+        console.error(error);
+    }
+});
